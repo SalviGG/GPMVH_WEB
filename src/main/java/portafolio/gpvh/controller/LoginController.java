bc457a9ed8c8ff900f6f054ef5d9794bcea0d602
sgutierrez <kei.miskatonic@gmail.com>
2018-10-09 12:07:11 -0300

    Baseline updated2

---
+++ src/main/java/portafolio/gpvh/controller/LoginController.java
@@ -0,0 +1,11 @@
+package portafolio.gpvh.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+
+}
